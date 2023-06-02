dfb1ceb7ec010380d51e250ad78098bc271317a5
thangnvph4303 <vanthang2003az@gmail.com>
2023-06-02 15:29:04 +0700

    Signup

--- FrontEnd/src/app/pages/signup/signup.component.ts
+++ FrontEnd/src/app/pages/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from './../../services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
+ constructor(private fb: FormBuilder, private authService: AuthService){
 
+  }
+  formSignup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ['', [Validators.required]]
+  }, { Validators: this.checkPassword})
+
+ 
+
+  checkPassword(form: FormGroup){
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+    if(password === confirmPassword) return null
+    return { notMatch: true};
+  }
+
+  onHandleSubmit(){
+    if(this.formSignup.valid){
+      this.authService.signup(this.formSignup.value).subscribe(data => {
+        console.log(data);
+        
+      })
+    }
+  }
 }
