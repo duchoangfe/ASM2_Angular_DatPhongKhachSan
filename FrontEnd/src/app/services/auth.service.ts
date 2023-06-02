46fb3eb7eda6d59e9e8c00ac1a2bda30183cb383
duchoangfe <duchoangfe@gmail.com>
2023-06-03 02:19:37 +0700

    refs: AA-7

--- FrontEnd/src/app/services/auth.service.ts
+++ FrontEnd/src/app/services/auth.service.ts
@@ -1,18 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
-
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class AuthService {
+    constructor(private http: HttpClient) {}
+    signup(user: any) {
+        return this.http.post('http://localhost:8080/api/signup', user).pipe(
+            catchError((error) => {
+                if (error.status === 400) {
+                    alert('Email đã tồn tại');
+                }
+                return throwError(error);
+            })
+        );
+    }
 
-  constructor(private http: HttpClient) { }
-
-  signup(user: any): Observable<any>{
-    console.log(user);
-    
-    return this.http.post('http://localhost:8080/api/signup', user)
-  }
+    signin(user: any) {
+        return this.http.post('http://localhost:8080/api/signin', user).pipe(
+            catchError((error) => {
+                if (error.status === 400) {
+                    alert('Tài Khoản không tồn tại');
+                } else if (error.status === 401) {
+                    alert('Sai Mật Khẩu');
+                }
+                return throwError(error);
+            })
+        );
+    }
 }
