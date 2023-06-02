11550a683e8e7e30a6b83718ae1a1ae38685e746
duchoangfe <duchoangfe@gmail.com>
2023-06-03 02:20:46 +0700

    git commit -m "AA-7 <signin>"

--- FrontEnd/src/app/pages/signin/signin.component.ts
+++ FrontEnd/src/app/pages/signin/signin.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from './../../services/auth.service';
 
 @Component({
-  selector: 'app-signin',
-  templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.scss']
+    selector: 'app-signin',
+    templateUrl: './signin.component.html',
+    styleUrls: ['./signin.component.scss'],
 })
 export class SigninComponent {
+    userData: any;
+    constructor(private fb: FormBuilder, private authService: AuthService) {
+        const storedData = localStorage.getItem('userData');
+        if (storedData) {
+            this.userData = JSON.parse(storedData);
+        }
+    }
 
+    formSignin = this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required]],
+    });
+
+    onHandleSubmit() {
+        if (this.formSignin.valid) {
+            this.authService.signin(this.formSignin.value).subscribe((data) => {
+                alert('Đăng Nhập Thành Công !');
+                localStorage.setItem('userData', JSON.stringify(data));
+            });
+        }
+    }
 }
--- FrontEnd/src/app/pages/signup/signup.component.ts
+++ FrontEnd/src/app/pages/signup/signup.component.ts
@@ -3,36 +3,34 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from './../../services/auth.service';
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+    selector: 'app-signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent {
- constructor(private fb: FormBuilder, private authService: AuthService){
+    constructor(private fb: FormBuilder, private authService: AuthService) {}
+    formSignup = this.fb.group(
+        {
+            name: ['', [Validators.required]],
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.minLength(6)]],
+            confirmPassword: ['', [Validators.required]],
+        },
+        { Validators: this.checkPassword }
+    );
 
-  }
-  formSignup = this.fb.group({
-    name: ['', [Validators.required]],
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
-    confirmPassword: ['', [Validators.required]]
-  }, { Validators: this.checkPassword})
-
- 
-
-  checkPassword(form: FormGroup){
-    const password = form.get('password')?.value;
-    const confirmPassword = form.get('confirmPassword')?.value;
-    if(password === confirmPassword) return null
-    return { notMatch: true}
-  }
-
-  onHandleSubmit(){
-    if(this.formSignup.valid){
-      this.authService.signup(this.formSignup.value).subscribe(data => {
-        console.log(data);
-        
-      })
+    checkPassword(form: FormGroup) {
+        const password = form.get('password')?.value;
+        const confirmPassword = form.get('confirmPassword')?.value;
+        if (password === confirmPassword) return null;
+        return { notMatch: true };
     }
-  }
+
+    onHandleSubmit() {
+        if (this.formSignup.valid) {
+            this.authService.signup(this.formSignup.value).subscribe((data) => {
+                alert('Đăng Ký Thành Công !');
+            });
+        }
+    }
 }
