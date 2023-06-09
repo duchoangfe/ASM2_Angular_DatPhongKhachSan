cd0f86d3dfaae1be59bdac11dd1417bdcbe109c1
thangnvph4303 <vanthang2003az@gmail.com>
2023-06-10 02:07:13 +0700

    feat: Fix List Booking

--- FrontEnd/src/app/components/booking-room/booking-room.component.ts
+++ FrontEnd/src/app/components/booking-room/booking-room.component.ts
@@ -1,31 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { IRoom } from 'src/app/interfaces/Room';
 import { RoomService } from 'src/app/services/room.service';
+
 @Component({
-    selector: 'app-booking-room',
-    templateUrl: './booking-room.component.html',
-    styleUrls: ['./booking-room.component.scss'],
+  selector: 'app-booking-room',
+  templateUrl: './booking-room.component.html',
+  styleUrls: ['./booking-room.component.scss']
 })
-export class BookingRoomComponent {
-    isLoggedIn: boolean = false;
-    _id: any;
-    name: any;
-    email: any;
-    hasUserInfo: any;
-    rooms: IRoom[] = [];
-    constructor(private roomService: RoomService) {
-        this.roomService.getRoomById(this._id).subscribe(
-            (data: any) => {
-                console.log(data._id);
-            },
-            (error) => console.log(error.message)
-        );
-    }
+export class BookingRoomComponent implements OnInit {
+  isLoggedIn: boolean = false;
+  _id: any;
+  name: any;
+  email: any;
+  hasUserInfo: any;
+  rooms: IRoom[] = [];
 
-    ngOnInit() {
-        this.name = localStorage.getItem('name');
-        this.email = localStorage.getItem('email');
+  constructor(private roomService: RoomService, private route: ActivatedRoute) { }
 
-        this.hasUserInfo = this.name ? true : false;
-    }
+  ngOnInit() {
+    this.name = localStorage.getItem('name');
+    this.email = localStorage.getItem('email');
+    this.hasUserInfo = this.name ? true : false;
+
+    this.route.paramMap.subscribe(param => {
+      const _id = param.get('id');
+      console.log(_id);
+
+      this.roomService.getRoomById(_id as any).subscribe((data: any) => {
+        this.rooms = [data.room];
+
+ 
+      });
+    });
+  }
 }
