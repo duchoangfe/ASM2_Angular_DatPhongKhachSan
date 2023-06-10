4a530286b04d37a34b0e719a1b329f8bb2f12c96
duchoangfe <duchoangfe@gmail.com>
2023-06-11 02:04:58 +0700

    [UPDATE] booking room UI

--- FrontEnd/src/app/components/booking-room/booking-room.component.ts
+++ FrontEnd/src/app/components/booking-room/booking-room.component.ts
@@ -4,34 +4,35 @@ import { IRoom } from 'src/app/interfaces/Room';
 import { RoomService } from 'src/app/services/room.service';
 
 @Component({
-  selector: 'app-booking-room',
-  templateUrl: './booking-room.component.html',
-  styleUrls: ['./booking-room.component.scss']
+    selector: 'app-booking-room',
+    templateUrl: './booking-room.component.html',
+    styleUrls: ['./booking-room.component.scss'],
 })
 export class BookingRoomComponent implements OnInit {
-  isLoggedIn: boolean = false;
-  _id: any;
-  name: any;
-  email: any;
-  hasUserInfo: any;
-  rooms: IRoom[] = [];
+    isLoggedIn: boolean = false;
+    _id: any;
+    name: any;
+    email: any;
+    hasUserInfo: any;
+    rooms: IRoom[] = [];
 
-  constructor(private roomService: RoomService, private route: ActivatedRoute) { }
+    constructor(
+        private roomService: RoomService,
+        private route: ActivatedRoute
+    ) {}
 
-  ngOnInit() {
-    this.name = localStorage.getItem('name');
-    this.email = localStorage.getItem('email');
-    this.hasUserInfo = this.name ? true : false;
+    ngOnInit() {
+        this.name = localStorage.getItem('name');
+        this.email = localStorage.getItem('email');
+        this.hasUserInfo = this.name ? true : false;
 
-    this.route.paramMap.subscribe(param => {
-      const _id = param.get('id');
-      console.log(_id);
+        this.route.paramMap.subscribe((param) => {
+            const _id = param.get('id');
+            console.log(_id);
 
-      this.roomService.getRoomById(_id as any).subscribe((data: any) => {
-        this.rooms = [data.room];
-
- 
-      });
-    });
-  }
+            this.roomService.getRoomById(_id as any).subscribe((data: any) => {
+                this.rooms = [data.room];
+            });
+        });
+    }
 }
